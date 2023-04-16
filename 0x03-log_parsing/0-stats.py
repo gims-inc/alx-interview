ebeda56bfb0dd09549552df666b2304405a6b41b
gims-inc <gabrielmusanga@gmail.com>
2023-04-16 07:55:34 +0300

    stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,29 +6,30 @@ codes = {'200': 0, '301': 0, '400': 0, '401': 0,
 file_size = 0
 
 try:
-    while True:
+    while sys.stdin:
         input_line = sys.stdin.readline()
         section = input_line.split()
 
         status_code = section[-2]
         size = section[-1]
 
-        if status_code in codes.keys():
-            codes[status_code] += 1
-
+        if status_code:
+            if status_code in codes.keys():
+                codes[status_code] += 1
+     
+        file_size += int(size)
+        
         for _ in range(10):
-            file_size += int(size)
-
-        print('File size: {}'.format(file_size))
-        for key, value in sorted(codes.items()):
-            if value > 0:
-                print('{}: {}'.format(key, value))
+            print("File size: {}".format(file_size))
+            for key, value in sorted(codes.items()):
+                if value > 0:
+                    print('{}: {}'.format(key, value))
 
 except KeyboardInterrupt:
     pass
 
 finally:
-    print('File size: {}'.format(file_size))
+    print("File size: {}".format(file_size))
     for key, value in sorted(codes.items()):
         if value > 0:
             print('{}: {}'.format(key, value))
