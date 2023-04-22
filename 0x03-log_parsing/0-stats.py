934764be89c6356fa95bce1304885e1bbb422cee
gims-inc <gabrielmusanga@gmail.com>
2023-04-22 12:52:24 +0300

    stats --

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,6 +4,17 @@ import sys
 codes = {'200': 0, '301': 0, '400': 0, '401': 0,
          '403': 0, '404': 0, '405': 0, '500': 0}
 file_size = 0
+count = 0
+
+
+def print_info():
+    """print input statistics
+    """
+    print("File size: {}".format(file_size))
+    for key, val in sorted(codes.items()):
+        if val > 0:
+            print("{}: {}".format(key, val))
+
 
 try:
     while sys.stdin:
@@ -12,24 +23,16 @@ try:
 
         status_code = section[-2]
         size = section[-1]
+        count += 1
 
         if status_code:
             if status_code in codes.keys():
                 codes[status_code] += 1
-
             file_size += int(size)
 
-        for _ in range(10):
-            print("File size: {}".format(file_size))
-            for key, value in sorted(codes.items()):
-                if value > 0:
-                    print('{}: {}'.format(key, value))
-
+        if count % 10 == 0:
+            print_info()
 except KeyboardInterrupt:
-    pass
-
-finally:
-    print("File size: {}".format(file_size))
-    for key, value in sorted(codes.items()):
-        if value > 0:
-            print('{}: {}'.format(key, value))
+    print_info()
+    raise
+print_info()
