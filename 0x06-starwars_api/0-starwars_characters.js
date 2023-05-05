ddb5c209d13c260ec6fde50ec0eca17500ec9b7b
gims-inc <gabrielmusanga@gmail.com>
2023-05-05 20:31:49 +0300

    promisify

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,29 +2,38 @@
 // script that prints all characters of a Star Wars movie.
 
 const request = require('request');
+const util = require('util');
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request(url, function (err, res, body) {
-  if (!err && res.statusCode === 200) {
-    const data = JSON.parse(body);
-    const characterUrls = data.characters;
+// Convert the callback-based request method to a Promise-based method
+const requestPromise = util.promisify(request);
 
-    const ids = characterUrls.map(url => parseInt(url.split('/').slice(-2, -1)[0]));
-    const sortedids = ids.sort((a, b) => a - b);
+(async function () {
+  try {
+    const { statusCode, body } = await requestPromise(url);
 
-    sortedids.forEach(id => {
-      request(`https://swapi-api.alx-tools.com/api/people/${id}/`, function (err, res, body) {
-        if (!err && res.statusCode === 200) {
+    if (statusCode === 200) {
+      const data = JSON.parse(body);
+      const characterUrls = data.characters;
+      const ids = characterUrls.map(url => parseInt(url.split('/').slice(-2, -1)[0]));
+      const sortedIds = ids.sort((a, b) => a - b);
+
+      for (const id of sortedIds) {
+        const { statusCode, body } = await requestPromise(`https://swapi-api.alx-tools.com/api/people/${id}/`);
+
+        if (statusCode === 200) {
           const data = JSON.parse(body);
           const charactername = data.name;
           console.log(charactername);
         } else {
-          console.log(err);
+          console.log(`Error: Received status code ${statusCode}`);
         }
-      });
-    });
-  } else {
-    console.log(`Error: ${err}`);
+      }
+    } else {
+      console.log(`Error: Received status code ${statusCode}`);
+    }
+  } catch (error) {
+    console.log(`Error: ${error}`);
   }
-});
+})();
