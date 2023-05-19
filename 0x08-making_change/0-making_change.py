8bea4ecc1f76166c87dd9656c5210aafefcb6145
gims-inc <gabrielmusanga@gmail.com>
2023-05-19 03:14:53 +0300

    pycodestyle ++total

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,31 +2,34 @@
 """Given a pile of coins of different values,
 determine the fewest number of coins needed
 to meet a given amount total.
-"""    
-def makeChange(coins, amount):
+"""
+
+
+def makeChange(coins, total):
     """Return the fewest number of coins needed to
     make change for the given amount.
     """
     # Sort the coins in descending order
     coins.sort(reverse=True)
-    
+
     # Initialize the count of coins needed
     count = 0
-    
+
     # Iterate through each coin denomination
     for coin in coins:
-        # Check if the coin can be used to make up the remaining amount
-        if coin <= amount:
-            # Calculate the number of coins needed for the current coin denomination
-            numCoins = amount // coin
+        # Check if the coin can be used to make up the remaining total
+        if coin <= total:
+            # Calculate the number of coins needed for the current coin
+            # denomination
+            numCoins = total // coin
             count += numCoins
-            
-            # Update the remaining amount
-            amount -= numCoins * coin
-    
-    # If there is still remaining amount, it cannot be made up with the given coin denominations
-    if amount != 0:
+
+            # Update the remaining total
+            total -= numCoins * coin
+
+    # If there is still remaining total, it cannot be made up with the given
+    # coin denominations
+    if total != 0:
         return -1
-    
+
     return count
-    
