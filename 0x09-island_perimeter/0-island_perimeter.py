a0597ac0caae44df7f0e66e127e0be1ffa1f27d0
gims-inc <gabrielmusanga@gmail.com>
2023-05-25 23:31:14 +0300

    soln-1 --

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,23 +8,45 @@
     grid is rectangular, with its width and height not exceeding 100
 """
 
+def count_adjacent_edges(grid, i, j):
+    """
+    Counts the number of adjacent edges around a land cell in the grid.
+    
+    Args:
+        grid (list): A list of lists representing the grid.
+        i (int): The row index of the land cell.
+        j (int): The column index of the land cell.
+        
+    Returns:
+        int: The count of adjacent edges.
+    """
+    count = 0
+    if j > 0 and grid[i][j - 1] == 1:
+        count += 1
+    if i > 0 and grid[i - 1][j] == 1:
+        count += 1
+    # Check other adjacent cells if needed
+
+    return count
+
 
 def island_perimeter(grid):
-    """Return the perimeter of the islands in grid.
-    args:
-        grid (list): of list of integers:
     """
-    width = len(grid[0])
-    height = len(grid)
-    edges = 0
-    size = 0
+    Returns the perimeter of the islands in the given grid.
+    
+    Args:
+        grid (list): A list of lists representing the grid.
+        
+    Returns:
+        int: The perimeter of the islands.
+    """
+    width, height = len(grid[0]), len(grid)
+    edges, size = 0, 0
 
     for i in range(height):
         for j in range(width):
             if grid[i][j] == 1:
                 size += 1
-                if (j > 0 and grid[i][j - 1] == 1):
-                    edges += 1
-                if (i > 0 and grid[i - 1][j] == 1):
-                    edges += 1
+                edges += count_adjacent_edges(grid, i, j)
+
     return size * 4 - edges * 2
