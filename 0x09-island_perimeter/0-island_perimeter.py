b735caa128b23cfd88c6aa69e2616ea4cb60882e
gims-inc <gabrielmusanga@gmail.com>
2023-05-25 23:21:19 +0300

    soln-1

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""grid is a list of list of integers:
+
+    0 represents water
+    1 represents land
+    Each cell is square, with a side length of 1
+    Cells are connected horizontally/vertically (not diagonally).
+    grid is rectangular, with its width and height not exceeding 100
+"""
+
+
+def island_perimeter(grid):
+    """Return the perimeter of the islands in grid.
+    args:
+        grid (list): of list of integers:
+    """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
